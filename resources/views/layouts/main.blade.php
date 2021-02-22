3006502bd306883ddeeaa27a46b24532f18f3c0b
williamtruppi <williamt@hotmail.it>
2021-02-22 16:03:46 +0100

    faker installato

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,14 +1,3 @@
-<div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    @yield("title")
-                </div>
-
-                <div class="links">
-                    <a href="{{route('home')}}">Home</a>
-                    <a href="{{route('employees')}}">Employee</a>
-                    <a href="{{route('about')}}">About</a>
-                    
-                </div>
-            </div>
-        </div>
+<main>
+    @yield("content")
+</main>
